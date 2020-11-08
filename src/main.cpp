0e69966095ec011393ab39987a6c699cea092da4
Pablo Varela <parapavlo@hotmail.com>
2020-11-09 00:52:42 +0100

    Four temperatures in display: Exterior (channel 884965), Salon (chanel 220821), Alén (channel 641875) &Eder (channel 1183276)

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <ThingSpeak.h>
 #include <ESP8266WiFi.h>
 #include "..\..\Credentials\CredentialsPvarela.h"
+#include "..\..\Credentials\ThingSpeak_Personal_Channels.h"
 //#include <ArduinoJson.h>
 #include <ESP8266HTTPClient.h>
 #include <WiFiEspUdp.h>
@@ -18,6 +19,32 @@ EasyNTPClient ntpClient(udp, "pool.ntp.org", ((5*60*60)+(30*60))); // IST = GMT 
 #define TFT_CS    D3     // TFT CS  pin is connected to NodeMCU pin D4 (GPIO0)
 #define TFT_DC    D2     // TFT DC  pin is connected to NodeMCU pin D4 (GPIO4)
 
+#define NUM_TEMPERATURES		4
+#define SIZE_AREA_TEMPERATURES 	ST7735_TFTHEIGHT_160/NUM_TEMPERATURES
+
+#define LINE_1_POSITION 			1*(ST7735_TFTHEIGHT_160/(NUM_TEMPERATURES))-2
+#define LINE_2_POSITION 			2*(ST7735_TFTHEIGHT_160/(NUM_TEMPERATURES))-2
+#define LINE_3_POSITION 			3*(ST7735_TFTHEIGHT_160/(NUM_TEMPERATURES))-2
+#define TITLE_1_POSITION			4 //8
+#define TITLE_2_POSITION			LINE_1_POSITION + 4
+#define TITLE_3_POSITION			LINE_2_POSITION + 4
+#define TITLE_4_POSITION			LINE_3_POSITION + 4
+#define SAMPLE_TIME_1_POSITION 		TITLE_1_POSITION
+#define SAMPLE_TIME_2_POSITION 		TITLE_2_POSITION
+#define SAMPLE_TIME_3_POSITION 		TITLE_3_POSITION
+#define SAMPLE_TIME_4_POSITION 		TITLE_4_POSITION
+#define TEMPERATURE_Y_1_POSITION 		LINE_1_POSITION - (SIZE_AREA_TEMPERATURES/2)
+#define TEMPERATURE_Y_2_POSITION 		LINE_2_POSITION - (SIZE_AREA_TEMPERATURES/2)
+#define TEMPERATURE_Y_3_POSITION 		LINE_3_POSITION - (SIZE_AREA_TEMPERATURES/2)
+#define TEMPERATURE_Y_4_POSITION 		ST7735_TFTHEIGHT_160 - (SIZE_AREA_TEMPERATURES/2)
+#define TEMPERATURE_X_1_POSITION		35
+#define TEMPERATURE_X_2_POSITION		TEMPERATURE_X_1_POSITION
+#define TEMPERATURE_X_3_POSITION		TEMPERATURE_X_1_POSITION
+#define TEMPERATURE_X_4_POSITION		TEMPERATURE_X_1_POSITION
+#define TEMPERATURE_Y_CIRCLE_POSITION	TEMPERATURE_X_1_POSITION + 54
+#define TEMPERATURE_Y_UNITS_POSITION	TEMPERATURE_X_1_POSITION + 60
+
+
 //CS D8
 //MOSI D7
 //SCK D5
@@ -29,15 +56,9 @@ Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
 
 static float tempExterior = -1.9;
 static float tempSalon = 10;
-static float tempHabitacion = 10;
+static float tempAlen = 10;
+static float tempEder = 10;
 
-unsigned long channelTempExterior = 884965;            // Channel ID
-const char * thingSpeakReadAPIKey1 = "K79HY3S0AFG0BJKB"; // Read API Key
-unsigned long channelTempSalon = 220821;            // Channel ID
-const char * thingSpeakReadAPIKey2 = "Q9R1021ONI8IOV5E"; // Read API Key
-unsigned long channelTempInterior = 641875;            // Channel ID
-const char * thingSpeakReadAPIKey3 = "NZMV7DRIIPZO6ART"; // Read API Key
-const int FieldNumber1 = 1;  // The field you wish to read
 WiFiClient  client;
 int statusCode;
 
@@ -60,33 +81,23 @@ void setup(void)
 
   tft.initR(INITR_BLACKTAB);     // initialize a ST7735S chip, black tab
   tft.fillScreen(ST77XX_BLACK);  // fill screen with black color
-//  tft.drawFastHLine(0, 2 ,  tft.width(), ST77XX_CYAN);   // draw horizontal white line at position (0, 30)
-  tft.drawFastHLine(0, 53 ,  tft.width(), ST77XX_CYAN);   // draw horizontal white line at position (0, 30)
-  tft.drawFastHLine(0, 108 ,  tft.width(), ST77XX_CYAN);   // draw horizontal white line at position (0, 30)
 
-  if( 0 )
-  {  // connection error or device address wrong!
-    tft.setTextColor(ST77XX_RED, ST77XX_CYAN);   // set text color to red and black background
-    tft.setTextSize(2);         // text size = 2
-    tft.setCursor(5, 76);       // move cursor to position (5, 76) pixel
-    tft.print("Connection");
-    tft.setCursor(35, 100);     // move cursor to position (35, 100) pixel
-    tft.print("Error");
-    while(1);  // stay here
-  }
+  tft.drawFastHLine(0, LINE_1_POSITION ,  tft.width(), ST77XX_WHITE);   // draw horizontal white line at position (0, 30)
+  tft.drawFastHLine(0, LINE_2_POSITION ,  tft.width(), ST77XX_WHITE);   // draw horizontal white line at position (0, 30)
+  tft.drawFastHLine(0, LINE_3_POSITION ,  tft.width(), ST77XX_WHITE);   // draw horizontal white line at position (0, 30)
 
-  //tft.drawFastHLine(0, 80,  tft.width(), ST77XX_CYAN);  // draw horizontal white line at position (0, 76)
-  //tft.drawFastHLine(0, 120,  tft.width(), ST77XX_CYAN);  // draw horizontal white line at position (0, 122)
-  tft.setTextColor(ST77XX_CYAN, ST77XX_BLACK);     // set text color to red and black background
-  tft.setCursor(14, 8);              // move cursor to position (25, 39) pixel
-  tft.print("Exterior ");
-  tft.setTextColor(ST77XX_CYAN, ST77XX_BLACK);  // set text color to cyan and black background
-  tft.setCursor(22, 61);              // move cursor to position (34, 85) pixel
-  tft.print("Salon ");
-  tft.setTextColor(ST77XX_CYAN, ST7735_BLACK);  // set text color to green and black background
-  tft.setCursor(6, 112);              // move cursor to position (34, 131) pixel
-  tft.print("Dormitorio ");
-  tft.setTextSize(2);                 // text size = 2
+  tft.setTextColor(ST77XX_WHITE, ST77XX_BLACK);  // set text color to red and black background
+  tft.setCursor(14, TITLE_1_POSITION);
+  tft.print("EXTERIOR ");
+  tft.setTextColor(ST77XX_WHITE, ST77XX_BLACK);  // set text color to cyan and black background
+  tft.setCursor(22, TITLE_2_POSITION);
+  tft.print("SALON ");
+  tft.setTextColor(ST77XX_WHITE, ST7735_BLACK);  // set text color to green and black background
+  tft.setCursor(30, TITLE_3_POSITION);
+  tft.print("ALEN ");
+  tft.setTextColor(ST77XX_WHITE, ST7735_BLACK);  // set text color to green and black background
+  tft.setCursor(30, TITLE_4_POSITION);
+  tft.print("EDER ");
 
 }
 
@@ -94,25 +105,26 @@ void setup(void)
 void loop()
 {
   char _buffer[8];
-  char _buffer_2[16];
 
   uint16_t colorTempExterior;
   uint16_t colorTempSalon;
-  uint16_t colorTempHabitacion;
+  uint16_t colorTempAlen;
+  uint16_t colorTempEder;
   String tempExteriorTime;
   String tempSalonTime;
-  String tempHabitacionTime;
+  String tempAlenTime;
+  String tempEderTime;
   int delimiter_1, delimiter_2;
 
 
-    tempExterior = ThingSpeak.readFloatField(channelTempExterior, FieldNumber1, thingSpeakReadAPIKey1);
+    tempExterior = ThingSpeak.readFloatField(channelTempExterior, FieldNumber1, thingSpeakReadAPIKey_Exterior);
 	statusCode = ThingSpeak.getLastReadStatus();
 	if (statusCode == 200)
 	{
 	  Serial.print("Temperature exterior ");
 	  Serial.print(tempExterior);
 	  Serial.print("ºC at ");
-	  tempExteriorTime = ThingSpeak.readCreatedAt(channelTempExterior, thingSpeakReadAPIKey1);
+	  tempExteriorTime = ThingSpeak.readCreatedAt(channelTempExterior, thingSpeakReadAPIKey_Exterior);
 	  delimiter_1 = tempExteriorTime.indexOf("T");
 	  delimiter_2 = tempExteriorTime.indexOf("+");
 	  tempExteriorTime = tempExteriorTime.substring(delimiter_1 + 1, delimiter_2);
@@ -125,14 +137,14 @@ void loop()
 
 
 	delay(100);
-    tempSalon = ThingSpeak.readFloatField(channelTempSalon, FieldNumber1, thingSpeakReadAPIKey2);
+    tempSalon = ThingSpeak.readFloatField(channelTempSalon, FieldNumber1, thingSpeakReadAPIKey_Salon);
 	statusCode = ThingSpeak.getLastReadStatus();
 	if (statusCode == 200)
 	{
 	  Serial.print("Temperature Salon ");
 	  Serial.print(tempSalon);
 	  Serial.print("ºC at ");
-	  tempSalonTime= ThingSpeak.readCreatedAt(channelTempSalon, thingSpeakReadAPIKey2);
+	  tempSalonTime= ThingSpeak.readCreatedAt(channelTempSalon, thingSpeakReadAPIKey_Salon);
 	  delimiter_1 = tempSalonTime.indexOf("T");
 	  delimiter_2 = tempSalonTime.indexOf("+");
 	  tempSalonTime = tempSalonTime.substring(delimiter_1 + 1, delimiter_2);
@@ -143,18 +155,18 @@ void loop()
 	  Serial.println("Unable to read channel / No internet connection");
 	}
 	delay(100);
-    tempHabitacion  = ThingSpeak.readFloatField(channelTempInterior, FieldNumber1, thingSpeakReadAPIKey3);
+    tempAlen  = ThingSpeak.readFloatField(channelTempAlen, FieldNumber1, thingSpeakReadAPIKey_Alen);
 	statusCode = ThingSpeak.getLastReadStatus();
 	if (statusCode == 200)
 	{
-	  Serial.print("Temperature Interior ");
-	  Serial.print(tempHabitacion);
+	  Serial.print("Temperature Alén ");
+	  Serial.print(tempAlen);
 	  Serial.print("ºC at ");
-	  tempHabitacionTime= ThingSpeak.readCreatedAt(channelTempInterior, thingSpeakReadAPIKey3);
-	  delimiter_1 = tempHabitacionTime.indexOf("T");
-	  delimiter_2 = tempHabitacionTime.indexOf("+");
-	  tempHabitacionTime = tempHabitacionTime.substring(delimiter_1 + 1, delimiter_2);
-	  Serial.println(tempHabitacionTime);
+	  tempAlenTime= ThingSpeak.readCreatedAt(channelTempAlen, thingSpeakReadAPIKey_Alen);
+	  delimiter_1 = tempAlenTime.indexOf("T");
+	  delimiter_2 = tempAlenTime.indexOf("+");
+	  tempAlenTime = tempAlenTime.substring(delimiter_1 + 1, delimiter_2);
+	  Serial.println(tempAlenTime);
 
 	}
 	else
@@ -162,6 +174,24 @@ void loop()
 	  Serial.println("Unable to read channel / No internet connection");
 	}
 	delay(100);
+    tempEder  = ThingSpeak.readFloatField(channelTempEder, FieldNumber1, thingSpeakReadAPIKey_Eder);
+	statusCode = ThingSpeak.getLastReadStatus();
+	if (statusCode == 200)
+	{
+	  Serial.print("Temperature Eder ");
+	  Serial.print(tempEder);
+	  Serial.print("ºC at ");
+	  tempEderTime= ThingSpeak.readCreatedAt(channelTempEder, thingSpeakReadAPIKey_Eder);
+	  delimiter_1 = tempEderTime.indexOf("T");
+	  delimiter_2 = tempEderTime.indexOf("+");
+	  tempEderTime = tempEderTime.substring(delimiter_1 + 1, delimiter_2);
+	  Serial.println(tempEderTime);
+
+	}
+	else
+	{
+	  Serial.println("Unable to read channel / No internet connection");
+	}
 
   if(tempExterior >= 37.0)
   {
@@ -229,83 +259,129 @@ void loop()
 		colorTempSalon = ST77XX_WHITE;
 	}
 
-  if(tempHabitacion >= 30.0)
+  if(tempAlen >= 30.0)
     {
-  	  	colorTempHabitacion = ST77XX_MAGENTA;
+  	  	colorTempAlen = ST77XX_MAGENTA;
     }
-    else if((tempHabitacion < 30) && (tempHabitacion >= 27))
+    else if((tempAlen < 30) && (tempAlen >= 27))
     {
-    	colorTempHabitacion = ST77XX_RED;
+    	colorTempAlen = ST77XX_RED;
     }
-    else if((tempHabitacion < 27) && (tempHabitacion >=25))
+    else if((tempAlen < 27) && (tempAlen >=25))
     {
-    	colorTempHabitacion = ST77XX_ORANGE;
+    	colorTempAlen = ST77XX_ORANGE;
     }
-	else if((tempHabitacion < 25) && (tempHabitacion >=22.5))
+	else if((tempAlen < 25) && (tempAlen >=22.5))
 	{
-		colorTempHabitacion = ST77XX_YELLOW;
+		colorTempAlen = ST77XX_YELLOW;
 	}
-	else if((tempHabitacion < 22.5) && (tempHabitacion >=20.5))
+	else if((tempAlen < 22.5) && (tempAlen >=20.5))
 	{
-		colorTempHabitacion = ST77XX_GREEN;
+		colorTempAlen = ST77XX_GREEN;
 	}
-	else if((tempHabitacion < 20.5) && (tempHabitacion >=18))
+	else if((tempAlen < 20.5) && (tempAlen >=18))
 	{
-		colorTempHabitacion = ST77XX_CYAN;
+		colorTempAlen = ST77XX_CYAN;
 	}
-	else if((tempHabitacion < 18) && (tempHabitacion >=14))
+	else if((tempAlen < 18) && (tempAlen >=14))
 	{
-		colorTempHabitacion = ST77XX_BLUE;
+		colorTempAlen = ST77XX_BLUE;
 	}
 	else
 	{
-		colorTempHabitacion = ST77XX_WHITE;
+		colorTempAlen = ST77XX_WHITE;
+	}
+
+  if(tempEder >= 30.0)
+  {
+		colorTempEder = ST77XX_MAGENTA;
+  }
+  else if((tempEder < 30) && (tempEder >= 27))
+  {
+	colorTempEder = ST77XX_RED;
+  }
+  else if((tempEder < 27) && (tempEder >=25))
+  {
+	colorTempEder = ST77XX_ORANGE;
+  }
+	else if((tempEder < 25) && (tempEder >=22.5))
+	{
+		colorTempEder = ST77XX_YELLOW;
+	}
+	else if((tempEder < 22.5) && (tempEder >=20.5))
+	{
+		colorTempEder = ST77XX_GREEN;
+	}
+	else if((tempEder < 20.5) && (tempEder >=18))
+	{
+		colorTempEder = ST77XX_CYAN;
+	}
+	else if((tempEder < 18) && (tempEder >=14))
+	{
+		colorTempEder = ST77XX_BLUE;
+	}
+	else
+	{
+		colorTempEder = ST77XX_WHITE;
 	}
 
   //print temperatura exterior;
   tft.setTextSize(1);
-  tft.setTextColor(ST77XX_CYAN, ST77XX_BLACK);     // set text color to red and black background
-  tft.setCursor(68, 8);              // move cursor to position (25, 39) pixel
+  tft.setTextColor(ST77XX_WHITE, ST77XX_BLACK);     // set text color to red and black background
+  tft.setCursor(68, SAMPLE_TIME_1_POSITION);
   tft.print(tempExteriorTime);
   tft.setTextSize(2);
   dtostrf(tempExterior, 2, 1, _buffer);
   tft.setTextColor(colorTempExterior, ST77XX_BLACK);  // set text color to yellow and black background
-  tft.setCursor(35, 32);
+  tft.setCursor(TEMPERATURE_X_1_POSITION, TEMPERATURE_Y_1_POSITION);
   tft.print(_buffer);
-  tft.drawCircle(89, 32, 2, colorTempExterior);  // print degree symbol ( ° )
-  tft.setCursor(95, 32);
+  tft.drawCircle(TEMPERATURE_Y_CIRCLE_POSITION, TEMPERATURE_Y_1_POSITION, 2, colorTempExterior);  // print degree symbol ( ° )
+  tft.setCursor(TEMPERATURE_Y_UNITS_POSITION, TEMPERATURE_Y_1_POSITION);
   tft.print("C");
 
   // print temperature Salon (in °C)
   tft.setTextSize(1);
-  tft.setTextColor(ST77XX_CYAN, ST77XX_BLACK);     // set text color to red and black background
-  tft.setCursor(60, 61);              // move cursor to position (25, 39) pixel
+  tft.setTextColor(ST77XX_WHITE, ST77XX_BLACK);     // set text color to red and black background
+  tft.setCursor(60, SAMPLE_TIME_2_POSITION );
   tft.print(tempSalonTime);
   tft.setTextSize(2);
   dtostrf(tempSalon, 2, 1, _buffer);
   tft.setTextColor(colorTempSalon, ST77XX_BLACK);  // set text color to magenta and black background
-  tft.setCursor(35, 84);
+  tft.setCursor(TEMPERATURE_X_2_POSITION, TEMPERATURE_Y_2_POSITION);
   tft.print(_buffer);
-  tft.drawCircle(89, 84, 2, colorTempSalon);  // print degree symbol ( ° )
-  tft.setCursor(95, 84);
+  tft.drawCircle(TEMPERATURE_Y_CIRCLE_POSITION, TEMPERATURE_Y_2_POSITION, 2, colorTempSalon);  // print degree symbol ( ° )
+  tft.setCursor(TEMPERATURE_Y_UNITS_POSITION, TEMPERATURE_Y_2_POSITION);
   tft.print("C");
 
-  // print temperature Habitacion (in °C)
+  // print temperature Alén (in °C)
   tft.setTextSize(1);
-  tft.setTextColor(ST77XX_CYAN, ST77XX_BLACK);     // set text color to red and black background
-  tft.setCursor(74, 112);              // move cursor to position (25, 39) pixel
-  tft.print(tempHabitacionTime);
+  tft.setTextColor(ST77XX_WHITE, ST77XX_BLACK);     // set text color to red and black background
+  tft.setCursor(63,SAMPLE_TIME_3_POSITION);
+  tft.print(tempAlenTime);
   tft.setTextSize(2);
-  dtostrf(tempHabitacion, 2, 1, _buffer);
-  tft.setTextColor(colorTempHabitacion, ST77XX_BLACK);  // set text color to orange and black background
-  tft.setCursor(35, 132);
+  dtostrf(tempAlen, 2, 1, _buffer);
+  tft.setTextColor(colorTempAlen, ST77XX_BLACK);  // set text color to orange and black background
+  tft.setCursor(TEMPERATURE_X_3_POSITION,TEMPERATURE_Y_3_POSITION);
   tft.print(_buffer);
-  tft.drawCircle(89, 132, 2, colorTempHabitacion);  // print degree symbol ( ° )
-  tft.setCursor(95, 132);
+  tft.drawCircle(TEMPERATURE_Y_CIRCLE_POSITION, TEMPERATURE_Y_3_POSITION, 2, colorTempAlen);  // print degree symbol ( ° )
+  tft.setCursor(TEMPERATURE_Y_UNITS_POSITION, TEMPERATURE_Y_3_POSITION);
+  tft.print("C");
+
+  // print temperature Eder (in °C)
+  tft.setTextSize(1);
+  tft.setTextColor(ST77XX_WHITE, ST77XX_BLACK);     // set text color to red and black background
+  tft.setCursor(63,SAMPLE_TIME_4_POSITION);
+  tft.print(tempEderTime);
+  tft.setTextSize(2);
+  dtostrf(tempEder, 2, 1, _buffer);
+  tft.setTextColor(colorTempEder, ST77XX_BLACK);  // set text color to orange and black background
+  tft.setCursor(TEMPERATURE_X_4_POSITION,TEMPERATURE_Y_4_POSITION);
+  tft.print(_buffer);
+  tft.drawCircle(TEMPERATURE_Y_CIRCLE_POSITION, TEMPERATURE_Y_4_POSITION, 2, colorTempEder);  // print degree symbol ( ° )
+  tft.setCursor(TEMPERATURE_Y_UNITS_POSITION, TEMPERATURE_Y_4_POSITION);
   tft.print("C");
 
 //  Serial.println(ntpClient.getUnixTime());
-
 
   delay(5000);    // wait a second
 }
