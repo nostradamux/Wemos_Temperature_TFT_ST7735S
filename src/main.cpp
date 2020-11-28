018e87400fc7bfe93d40a4c4207dcd92dc37cfd4
Pablo Varela <parapavlo@hotmail.com>
2020-11-28 17:11:27 +0100

    Getting current time via NTP Server and check how much time since last sample in every Sensor. If more than 10 minutes, time in display is shown in red to alert user

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,9 @@
 #include "..\..\Credentials\ThingSpeak_Personal_Channels.h"
 #include <ESP8266HTTPClient.h>
 #include <WiFiEspUdp.h>
-#include <EasyNTPClient.h>
+#include <Time.h>
+#include "NTPClient.h"
+#include <WiFiUdp.h>
 
 // ST7735 TFT module connections
 #define TFT_RST   D4
@@ -20,6 +22,7 @@
 
 #define NUM_TEMPERATURES		4
 #define SIZE_AREA_TEMPERATURES 	ST7735_TFTHEIGHT_160/NUM_TEMPERATURES
+#define MAX_SEC_ALLOWED_WITH_NO_SAMPLE	20*60 /*20 minutes with no update data will generate alert!*/
 
 #define LINE_1_POSITION 				1*(ST7735_TFTHEIGHT_160/(NUM_TEMPERATURES))-2
 #define LINE_2_POSITION 				2*(ST7735_TFTHEIGHT_160/(NUM_TEMPERATURES))-2
@@ -43,9 +46,15 @@
 #define TEMPERATURE_X_CIRCLE_POSITION	TEMPERATURE_X_1_POSITION + 54
 #define TEMPERATURE_X_UNITS_POSITION	TEMPERATURE_X_1_POSITION + 60
 
-WiFiEspUDP udp;
-EasyNTPClient ntpClient(udp, "pool.ntp.org", ((5*60*60)+(30*60))); // IST = GMT + 5:30
 Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
+// Define NTP Client to get time
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP);
+
+// Variables to save date and time
+String formattedCurrentDate;
+String dayStamp;
+String timeStamp;
 
 WiFiClient  client;
 int statusCode;
@@ -55,6 +64,54 @@ enum enumTemp{
 	enumAlenTemp,
 	enumEderTemp
 };
+
+time_t SetDateTime(int y, int m, int d, int h, int mi, int s  )
+{
+	tmElements_t DateTime ;
+	DateTime.Second = s;
+	DateTime.Minute = mi;
+	DateTime.Hour = h;
+	DateTime.Day = d ;
+	DateTime.Month = m ;
+	DateTime.Year = y -1970 ;
+
+	return makeTime(DateTime);
+}
+
+time_t convertToTime(String calTimestamp) {
+  struct tm tm;
+  String year = calTimestamp.substring(0, 4);
+  String month = calTimestamp.substring(5, 7);
+  if (month.startsWith("0")) {
+    month = month.substring(1);
+  }
+  String day = calTimestamp.substring(8, 10);
+  if (day.startsWith("0")) {
+    month = day.substring(1);
+  }
+  tm.tm_year = year.toInt();
+  tm.tm_mon = month.toInt();
+  tm.tm_mday = day.toInt();
+  String hour = calTimestamp.substring(11, 13);
+  tm.tm_hour = hour.toInt();
+  String min = calTimestamp.substring(14, 16);
+  tm.tm_min = min .toInt();
+  String sec = calTimestamp.substring(17, 19);
+  tm.tm_sec = sec.toInt();
+
+  return SetDateTime(tm.tm_year,tm.tm_mon, tm.tm_mday,tm.tm_hour,tm.tm_min,tm.tm_sec);
+}
+
+String getCurrentTime() {
+  while(!timeClient.update()) {
+    timeClient.forceUpdate();
+  }
+  // The formattedDate comes with the following format:
+  // 2018-05-28T16:00:13Z
+  // We need to extract date and time
+  return timeClient.getFormattedDate();
+
+}
 
 uint16_t SetColorTempTxt(enumTemp selection, float temperatureToEvaluate)
 {
@@ -133,6 +190,7 @@ uint16_t SetColorTempTxt(enumTemp selection, float temperatureToEvaluate)
 			}
 		break;
 	default:
+		resultColor = ST77XX_RED;
 		break;
 	}
 	return (resultColor);
@@ -175,11 +233,26 @@ void setup(void)
   tft.setCursor(30, TITLE_4_POSITION);
   tft.print("EDER ");
 
+  timeClient.begin();
+  timeClient.setTimeOffset(3600);
+
 }
+
+void printDateTime(time_t  t)
+{
+	Serial.print(day(t)) ;    Serial.print(+ "/") ;   Serial.print(month(t));   Serial.print(+ "/") ;
+	Serial.print(year(t));    Serial.print( " ") ;
+	Serial.print(hour(t));   Serial.print(+ ":") ;   Serial.print(minute(t));   Serial.print(":") ;
+	Serial.println(second(t));
+	delay(1000);
+}
+
 
 // main loop
 void loop()
 {
+	time_t  currentT,lastSampleSent;        // Date-Time variables
+
 	char _buffer[8];
 	uint16_t colorTempExterior;
 	uint16_t colorTempSalon;
@@ -189,29 +262,64 @@ void loop()
 	String tempSalonTime;
 	String tempAlenTime;
 	String tempEderTime;
-	static float tempExterior = -1.9;
-	static float tempSalon = 10;
-	static float tempAlen = 10;
-	static float tempEder = 10;
+	String tempExteriorTimeOld;
+	String tempSalonTimeOld;
+	String tempAlenTimeOld;
+	String tempEderTimeOld;
+	bool alertDisconnectionTempExterior = true;
+	bool alertDisconnectionTempSalon = true;
+	bool alertDisconnectionTempAlen = true;
+	bool alertDisconnectionTempEder = true;
+
+	static float tempExterior = -20;
+	static float tempSalon = 0.0;
+	static float tempAlen = 0.0;
+	static float tempEder = 0.0;
+	static float tempExteriorOld = tempExterior;
+	static float tempSalonOld = tempSalon;
+	static float tempAlenOld = tempAlen;
+	static float tempEderOld = tempEder;
 	int delimiter_1, delimiter_2;
+
+	Serial.println("\n\r\n\r");
+
+	/*Current time*/
+    formattedCurrentDate = getCurrentTime();
+	currentT = convertToTime(formattedCurrentDate);
+	Serial.print("Current time:");
+	Serial.println(formattedCurrentDate);
 
     /*Get temperature exterior*/
 	tempExterior = ThingSpeak.readFloatField(channelTempExterior, FieldNumber1, thingSpeakReadAPIKey_Exterior);
 	statusCode = ThingSpeak.getLastReadStatus();
 	if (statusCode == 200)
 	{
+	  tempExteriorTime = ThingSpeak.readCreatedAt(channelTempExterior, thingSpeakReadAPIKey_Exterior);
+	  lastSampleSent = convertToTime(tempExteriorTime);
 	  Serial.print("Temperature exterior ");
 	  Serial.print(tempExterior);
 	  Serial.print("ºC at ");
-	  tempExteriorTime = ThingSpeak.readCreatedAt(channelTempExterior, thingSpeakReadAPIKey_Exterior);
 	  delimiter_1 = tempExteriorTime.indexOf("T");
 	  delimiter_2 = tempExteriorTime.indexOf("+");
 	  tempExteriorTime = tempExteriorTime.substring(delimiter_1 + 1, delimiter_2);
-	  Serial.println(tempExteriorTime);
+	  Serial.print(tempExteriorTime);
+	  Serial.printf(". No sample during %d s\n\r",(int)(currentT-lastSampleSent));
+	  if((int)(currentT-lastSampleSent)> MAX_SEC_ALLOWED_WITH_NO_SAMPLE)
+	  {
+		  alertDisconnectionTempExterior = true;
+	  }
+	  else
+	  {
+		  alertDisconnectionTempExterior = false;
+	  }
+	  tempExteriorOld = tempExterior;
+	  tempExteriorTimeOld = tempExteriorTime;
 	}
 	else
 	{
 	  Serial.println("Unable to read channel / No internet connection");
+	  tempExterior = tempExteriorOld;
+	  tempExteriorTime = tempExteriorTimeOld;
 	}
 
     /*Get temperature Salon*/
@@ -220,18 +328,32 @@ void loop()
 	statusCode = ThingSpeak.getLastReadStatus();
 	if (statusCode == 200)
 	{
+	  tempSalonTime= ThingSpeak.readCreatedAt(channelTempSalon, thingSpeakReadAPIKey_Salon);
+	  lastSampleSent = convertToTime(tempSalonTime);
 	  Serial.print("Temperature Salon ");
 	  Serial.print(tempSalon);
 	  Serial.print("ºC at ");
-	  tempSalonTime= ThingSpeak.readCreatedAt(channelTempSalon, thingSpeakReadAPIKey_Salon);
 	  delimiter_1 = tempSalonTime.indexOf("T");
 	  delimiter_2 = tempSalonTime.indexOf("+");
 	  tempSalonTime = tempSalonTime.substring(delimiter_1 + 1, delimiter_2);
-	  Serial.println(tempSalonTime);
+	  Serial.print(tempSalonTime);
+	  Serial.printf(". No sample during %d s\n\r",(int)(currentT-lastSampleSent));
+	  if((int)(currentT-lastSampleSent)> MAX_SEC_ALLOWED_WITH_NO_SAMPLE)
+	  {
+		  alertDisconnectionTempSalon = true;
+	  }
+	  else
+	  {
+		  alertDisconnectionTempSalon = false;
+	  }
+	  tempSalonOld = tempSalon;
+	  tempSalonTimeOld = tempSalonTime;
 	}
 	else
 	{
 	  Serial.println("Unable to read channel / No internet connection");
+	  tempSalon = tempSalonOld;
+	  tempSalonTime = tempSalonTimeOld;
 	}
 
     /*Get temperature Alén*/
@@ -240,19 +362,32 @@ void loop()
 	statusCode = ThingSpeak.getLastReadStatus();
 	if (statusCode == 200)
 	{
+	  tempAlenTime= ThingSpeak.readCreatedAt(channelTempAlen, thingSpeakReadAPIKey_Alen);
+	  lastSampleSent = convertToTime(tempAlenTime);
 	  Serial.print("Temperature Alén ");
 	  Serial.print(tempAlen);
 	  Serial.print("ºC at ");
-	  tempAlenTime= ThingSpeak.readCreatedAt(channelTempAlen, thingSpeakReadAPIKey_Alen);
 	  delimiter_1 = tempAlenTime.indexOf("T");
 	  delimiter_2 = tempAlenTime.indexOf("+");
 	  tempAlenTime = tempAlenTime.substring(delimiter_1 + 1, delimiter_2);
-	  Serial.println(tempAlenTime);
-
+	  Serial.print(tempAlenTime);
+	  Serial.printf(". No sample during %d s\n\r",(int)(currentT-lastSampleSent));
+	  if((int)(currentT-lastSampleSent)> MAX_SEC_ALLOWED_WITH_NO_SAMPLE)
+	  {
+		  alertDisconnectionTempAlen = true;
+	  }
+	  else
+	  {
+		  alertDisconnectionTempAlen = false;
+	  }
+	  tempAlenOld = tempAlen;
+	  tempAlenTimeOld = tempAlenTime;
 	}
 	else
 	{
 	  Serial.println("Unable to read channel / No internet connection");
+	  tempAlen = tempAlenOld;
+	  tempAlenTime = tempAlenTimeOld;
 	}
 
     /*Get temperature Eder*/
@@ -261,19 +396,33 @@ void loop()
 	statusCode = ThingSpeak.getLastReadStatus();
 	if (statusCode == 200)
 	{
+	  tempEderTime= ThingSpeak.readCreatedAt(channelTempEder, thingSpeakReadAPIKey_Eder);
+	  lastSampleSent = convertToTime(tempEderTime);
 	  Serial.print("Temperature Eder ");
 	  Serial.print(tempEder);
 	  Serial.print("ºC at ");
-	  tempEderTime= ThingSpeak.readCreatedAt(channelTempEder, thingSpeakReadAPIKey_Eder);
 	  delimiter_1 = tempEderTime.indexOf("T");
 	  delimiter_2 = tempEderTime.indexOf("+");
 	  tempEderTime = tempEderTime.substring(delimiter_1 + 1, delimiter_2);
-	  Serial.println(tempEderTime);
+	  Serial.print(tempEderTime);
+	  Serial.printf(". No sample during %d s\n\r",(int)(currentT-lastSampleSent));
+	  if((int)(currentT-lastSampleSent)> MAX_SEC_ALLOWED_WITH_NO_SAMPLE)
+	  {
+		  alertDisconnectionTempEder = true;
+	  }
+	  else
+	  {
+		  alertDisconnectionTempEder = false;
+	  }
+	  tempEderOld = tempEder;
+	  tempEderTimeOld = tempEderTime;
 
 	}
 	else
 	{
 	  Serial.println("Unable to read channel / No internet connection");
+	  tempEder = tempEderOld;
+	  tempEderTime = tempEderTimeOld;
 	}
 
 	colorTempExterior = SetColorTempTxt(enumExteriorTemp, tempExterior);
@@ -283,7 +432,16 @@ void loop()
 
 	//print temperatura exterior;
 	tft.setTextSize(1);
-	tft.setTextColor(ST77XX_WHITE, ST77XX_BLACK);     // set text color to red and black background
+	if(alertDisconnectionTempExterior)
+	{
+		tft.setTextColor(ST77XX_RED, ST77XX_BLACK);     // set text color to red and black background
+		tempExteriorTime += "!!";
+	}
+	else
+	{
+		tft.setTextColor(ST77XX_WHITE, ST77XX_BLACK);     // set text color to white and black background
+		tempExteriorTime += "  ";
+	}
 	tft.setCursor(68, SAMPLE_TIME_1_POSITION);
 	tft.print(tempExteriorTime);
 	tft.setTextSize(2);
@@ -297,7 +455,16 @@ void loop()
 
 	// print temperature Salon (in °C)
 	tft.setTextSize(1);
-	tft.setTextColor(ST77XX_WHITE, ST77XX_BLACK);     // set text color to red and black background
+	if(alertDisconnectionTempSalon)
+	{
+		tft.setTextColor(ST77XX_RED, ST77XX_BLACK);     // set text color to red and black background
+		tempSalonTime += "!!";
+	}
+	else
+	{
+		tft.setTextColor(ST77XX_WHITE, ST77XX_BLACK);     // set text color to white and black background
+		tempSalonTime += "  ";
+	}
 	tft.setCursor(60, SAMPLE_TIME_2_POSITION );
 	tft.print(tempSalonTime);
 	tft.setTextSize(2);
@@ -311,7 +478,16 @@ void loop()
 
 	// print temperature Alén (in °C)
 	tft.setTextSize(1);
-	tft.setTextColor(ST77XX_WHITE, ST77XX_BLACK);     // set text color to red and black background
+	if(alertDisconnectionTempAlen)
+	{
+		tft.setTextColor(ST77XX_RED, ST77XX_BLACK);     // set text color to red and black background
+		tempAlenTime += "!!";
+	}
+	else
+	{
+		tft.setTextColor(ST77XX_WHITE, ST77XX_BLACK);     // set text color to white and black background
+		tempAlenTime += "  ";
+	}
 	tft.setCursor(63,SAMPLE_TIME_3_POSITION);
 	tft.print(tempAlenTime);
 	tft.setTextSize(2);
@@ -325,7 +501,16 @@ void loop()
 
 	// print temperature Eder (in °C)
 	tft.setTextSize(1);
-	tft.setTextColor(ST77XX_WHITE, ST77XX_BLACK);     // set text color to red and black background
+	if(alertDisconnectionTempEder)
+	{
+		tft.setTextColor(ST77XX_RED, ST77XX_BLACK);     // set text color to red and black background
+		tempEderTime += "!!";
+	}
+	else
+	{
+		tft.setTextColor(ST77XX_WHITE, ST77XX_BLACK);     // set text color to white and black background
+		tempEderTime += "  ";
+	}
 	tft.setCursor(63,SAMPLE_TIME_4_POSITION);
 	tft.print(tempEderTime);
 	tft.setTextSize(2);
